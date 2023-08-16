8aee4a5967d29510ea62692ed77c87d6aba5e9a2
Ryu Siwoo <61928790+thezoot3@users.noreply.github.com>
2023-08-16 11:12:19 +0000

    ㅁㄴㅇㄹ

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,1 +1,22 @@
-export {}
+import nav from './nav.module.scss';
+import { Link } from 'react-router-dom';
+function Footer() {
+    return (
+        <div className={nav.footer}>
+            <div className={nav.footer_container}>
+                <span className={nav.footer_project}>
+                    SEDA LOCKER
+                </span>
+                <span className={nav.footer_author}>
+                    gh:thezoot3
+                </span>
+            </div>
+            <div className={nav.footer_container}>
+                <span className={nav.footer_osl}>
+                    Open Source License
+                </span>
+            </div>
+            </div>
+    )
+}
+export default Footer;
